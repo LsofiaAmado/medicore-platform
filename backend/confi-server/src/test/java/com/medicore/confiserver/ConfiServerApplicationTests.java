28a06ab23a5e1cb211c069fab6b86b1e56bc73ae
Laura Sofia Amado <sofya3ama@gmail.com>
2026-08-06 23:42:44 -0500

    feat(infrastructure): configure Config Server, Eureka Discovery and API Gateway

---
+++ backend/confi-server/src/test/java/com/medicore/confiserver/ConfiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicore.confiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
