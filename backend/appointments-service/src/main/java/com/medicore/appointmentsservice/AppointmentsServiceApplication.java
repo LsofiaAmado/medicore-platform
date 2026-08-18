4da93e6eb171613d4f9d29ba0f420120d4c08300
Laura Sofia Amado <sofya3ama@gmail.com>
2026-08-18 18:08:46 -0500

    feat(appointments): add appointments service structure

--- backend/appointments-service/src/main/java/com/medicore/appointmentsservice/AppointmentsServiceApplication.java
+++ backend/appointments-service/src/main/java/com/medicore/appointmentsservice/AppointmentsServiceApplication.java
@@ -2,8 +2,13 @@ package com.medicore.appointmentsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMethodSecurity
 public class AppointmentsServiceApplication {
 
     public static void main(String[] args) {
---
+++ backend/appointments-service/src/main/java/com/medicore/appointmentsservice/security/JwtService.java
@@ -0,0 +1,56 @@
+package com.medicore.appointmentsservice.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Long extractUserId(String token) {
+
+        return extractAllClaims(token)
+                .get("userId", Long.class);
+    }
+
+    public String extractRole(String token) {
+
+        return extractAllClaims(token)
+                .get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
